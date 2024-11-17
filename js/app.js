ed23d86ac9ed011f89276ca345156afc66434263
Afroza Abedin <afrozaabedin79@gmail.com>
2024-11-17 18:49:02 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let incrementBtn = document.querySelector('.incrementBtn');
+let counterInput = document.querySelector('.counter input');
+
+function increment(){
+    let value = Number(counterInput.value);
+    counterInput.value = value + 1
+}
+incrementBtn.addEventListener('click', increment)
+ let decrementBtn = document.querySelector('.decrementBtn');
+
+function decrement(){
+    let value = Number(counterInput.value);
+    if (value > 1){
+     counterInput.value = value - 1}
+} 
+decrementBtn.addEventListener('click', decrement);
+
+ function convertToPositiveNum(){
+    let positiveNum = Math.abs(counterInput.value);
+    counterInput.value = positiveNum;
+ }
+
+counterInput.addEventListener('keyup',convertToPositiveNum);
